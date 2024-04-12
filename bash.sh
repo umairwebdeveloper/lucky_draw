6d556db39e38eab00121293d3a8d168ad14b0671
umairwebdeveloper <umairashraf5252@gmail.com>
2024-04-12 16:57:10 +0500

    fixed

--- bash.sh
+++ bash.sh
@@ -1,6 +1,5 @@
 echo "Building the project..."
-python3.9 -m pip install pypiwin32
-python3.9 -m pip install -r requirements.txt
+python3.9 -m pip3 install Django=4.2.2 
 
 echo "Collect Static..."
 python3.9 manage.py collectstatic --noinput --clear
