59b4b2140fea5a5a7823b467700255d7262cc6da
umairwebdeveloper <umairashraf5252@gmail.com>
2024-04-12 16:09:38 +0500

    fixed

--- bash.sh
+++ bash.sh
@@ -2,7 +2,7 @@
 
 # Build the project
 echo "Building the project..."
-python3.9 -m pip install -r requirements.txt
+python3.9 -m pip install django
 
 echo "Collect Static..."
 python3.9 manage.py collectstatic --noinput --clear
