ad71be4beff73637e2c60a95e906963266fd54f2
umairwebdeveloper <umairashraf5252@gmail.com>
2024-04-12 17:14:53 +0500

    fixed

--- build_file.sh
+++ build_file.sh
@@ -1,6 +1,6 @@
 echo "Building the project..."
-python3.8 -m pip install -r requirements.txt
+python3.9 -m pip install -r requirements.txt
 
 echo "Collect Static..."
-python3.8 manage.py collectstatic --noinput --clear
+python3.9 manage.py collectstatic --noinput --clear
 
