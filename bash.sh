df21c27d3b5d78495473e39f569a1eb1834f8e23
umairwebdeveloper <umairashraf5252@gmail.com>
2024-04-12 16:38:37 +0500

    fixed

--- bash.sh
+++ bash.sh
@@ -1,6 +1,5 @@
 echo "Building the project..."
-source venv/bin/activate
-python3.9 -m pip install -r requirements.txt
+python3.9 pip install -r requirements.txt
 
 echo "Collect Static..."
 python3.9 manage.py collectstatic --noinput --clear
