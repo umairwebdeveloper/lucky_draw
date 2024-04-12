547ddfebd52d12811b80c3bfa4b2d078bf0f54af
umairwebdeveloper <umairashraf5252@gmail.com>
2024-04-12 15:34:25 +0500

    fixed

---
+++ bash.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Build the project
+echo "Building the project..."
+python3.9 -m pip install -r requirements.txt
+
+echo "Collect Static..."
+python3.9 manage.py collectstatic --noinput --clear
+
--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-ai_im!)&!*+)*a2($bl5nak$gv@vhu^8m$oeftq7!xc01o#atm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', 'localhost', '127.0.0.1', '.now.sh']
 
 
 # Application definition
@@ -118,10 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
